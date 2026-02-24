8ce4762ee36b51a8030927f9f535dcd0b17bfd76
zErenTitan <eduardo18330@gmail.com>
2026-02-24 20:18:27 -0300

    v1

---
+++ src/main/java/org/example/repositories/ContaBancariaRepository.java
@@ -0,0 +1,11 @@
+package org.example.repositories;
+
+
+public class ContaBancariaRepository {
+    public ContaBancaria save(ContaBancaria ){
+        return new ContaBancaria();
+    }
+    public List<ContaBancaria> findAll(){
+        return ArrayList<>();
+    }
+}
